8f5c0e91a7bfc2a9c4faaa98bfd3dfcdf6d4bd32
Hajar Aswad <hajaraswadkom@gmail.com>
2022-06-07 08:27:53 +0700

    Hero Banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,22 +1,25 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import { urlFor } from '../lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
     return (
         <div className='hero-banner-container h-[40vh] bg-gray-200 pt-20 px-10'>
             <div>
-                <p>Small</p>
-                <h3 className='font-bold text-4xl'>Mid Text</h3>
-                <img src="" alt="hero-banner-image" />
+                <p>{heroBanner.smallText}</p>
+                <h3 className='font-bold text-4xl'>{heroBanner.midText}</h3>
+                <h1>{heroBanner.largeText1}</h1>
+                <img className='w-40' src={urlFor(heroBanner.image)} alt="hero-banner-image" />
 
                 <div>
                     <Link href="/product/ID">
-                        <button className='p-2 bg-red-700 rounded-lg text-white' type="button">Button Text</button>
+                        <button className='p-2 bg-red-700 rounded-lg text-white' type="button">{heroBanner.buttonText}</button>
                     </Link>
 
                     <div>
                         <h5 className='desc font-bold text-xl'>
-                            Lorem ipsum 
+                            Lorem ipsum
                         </h5>
                         <p>Desc</p>
                     </div>
