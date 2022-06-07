227fbdb44eaf3764aa5318985146b6441945817e
Hajar Aswad <hajaraswadkom@gmail.com>
2022-06-07 08:43:54 +0700

    Hero Banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,23 +5,23 @@ import { urlFor } from '../lib/client'
 
 const HeroBanner = ({ heroBanner }) => {
     return (
-        <div className='hero-banner-container h-[40vh] bg-gray-200 pt-20 px-10'>
-            <div>
+        <div className='hero-banner-container min-h-[40vh] bg-gray-200 pt-20 px-10 pb-10'>
+            <div className=''>
                 <p>{heroBanner.smallText}</p>
-                <h3 className='font-bold text-4xl'>{heroBanner.midText}</h3>
-                <h1>{heroBanner.largeText1}</h1>
-                <img className='w-40' src={urlFor(heroBanner.image)} alt="hero-banner-image" />
+                <h3 className='font-bold text-xl'>{heroBanner.midText}</h3>
+                <h1 className='font-bold text-8xl'>{heroBanner.largeText1}</h1>
+                <img className='w-40 ' src={urlFor(heroBanner.image)} alt="hero-banner-image" />
 
                 <div>
-                    <Link href="/product/ID">
-                        <button className='p-2 bg-red-700 rounded-lg text-white' type="button">{heroBanner.buttonText}</button>
+                    <Link href={`/product/${heroBanner.product}`}>
+                        <button className='p-2 bg-red-500 rounded-lg text-white' type="button">{heroBanner.buttonText}</button>
                     </Link>
 
-                    <div>
+                    <div className='float-right text-right'>
                         <h5 className='desc font-bold text-xl'>
-                            Lorem ipsum
+                            Description
                         </h5>
-                        <p>Desc</p>
+                        <p className='font-thin uppercase'>{heroBanner.desc}</p>
                     </div>
                 </div>
             </div>
