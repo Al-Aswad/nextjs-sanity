d053bac7da98d9a95e394e3d5af6c8048f02f9b2
Hajar Aswad <hajaraswadkom@gmail.com>
2022-06-07 14:22:02 +0700

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar-container flex justify-between px-10 py-4'>
+            <p>
+                <Link href="/">
+                    JSM Headphones
+                </Link>
+            </p>
+
+            <button type='button' className='relative'>
+                <AiOutlineShoppingCart  className='w-8 h-8'/>
+                <span className='cart-item-qty text-white absolute top-0 w-4 h-4 right-0 p-1 text-[12px] flex justify-center items-center bg-red-600 rounded-full'>1</span>
+            </button>
+        </div>
     )
 }
 
